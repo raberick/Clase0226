d9c5b109207c4ff644cbe349073e6f91cf12cd6d
albertolh97 <albertolh1997@gmail.com>
2020-02-26 13:19:47 +0100

    Final

--- Ordenar.py
+++ Ordenar.py
@@ -1,10 +1,10 @@
 def ord (lista):
-    orden=[]
+    orden=list(range(len(lista)))
     for i in range(0,len(lista)):
         a=min(lista)
         b=lista.index(min(lista))
         lista.pop(b)
-        orden.append(a)
+        orden[i]=a
     return orden
 
 print(ord([3,9,6]))
